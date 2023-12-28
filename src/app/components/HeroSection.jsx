fb21446b33fc5e6971ec584bb68ce6b6afd43acd
moewaizzz <moewai3127@gmail.com>
2023-12-28 07:09:06 +0630

    fix the animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,12 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-7 place-self-center text-center sm:text-left"
+        >
           <h1 className=" text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             Hello, I&apos;m {""}
             <TypeAnimation
@@ -28,10 +34,10 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg  mb-6 lg:text-xl">
+          {/* <p className="text-[#ADB7BE] text-base sm:text-lg  mb-6 lg:text-xl">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
             nihil expedita voluptas ea vel dolores?
-          </p>
+          </p> */}
           <div>
             <button className=" px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
               Hire me
@@ -45,8 +51,13 @@ const HeroSection = () => {
               </span>
             </Link>
           </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-5 place-self-center mt-4 lg:mt-0"
+        >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] relative lg:w-[400px] lg:h-[400px]">
             <Image
               src="/memoji.png"
@@ -56,7 +67,7 @@ const HeroSection = () => {
               className="absolute tansform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
-// import { motion, useInView } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
   {
@@ -31,12 +31,19 @@ const projectsData = [
 ];
 
 const ProjectsSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+      {/* <div className="grid md:grid-cols-3 gap-8 md:gap-12">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
@@ -47,7 +54,27 @@ const ProjectsSection = () => {
             previewUrl={project.previewUrl}
           />
         ))}
-      </div>
+      </div> */}
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {projectsData.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
     </section>
   );
 };
